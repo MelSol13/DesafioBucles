a7dc2c550e0772bcfd8ef2ddd096cb97f3ed03fd
Melissa Solano <m.solanof13@gmail.com>
2023-08-31 17:31:15 -0600

    first commit

---
+++ desafioBucles.js
@@ -0,0 +1,40 @@
+for (var i=1; i<=20; i++) {
+if(i%3===0){
+    console.log(i)
+}
+}
+
+for (var i=100; i>=1; i--) {
+    if(i%3===0){
+        console.log(i)
+    }
+}
+
+var lista= 4;
+for (var i= 4; i> -4 ; i-= 1.5){
+    console.log (i);
+}
+
+var numero=100;
+function sumatoria(numero){
+    var sum=0;
+
+for (var i=1; i<= numero; i++){
+    sum += i;
+    }
+    console.log('La sumatoria es => '+ sum);
+}
+
+sumatoria(numero);
+sumatoria(100);
+
+
+var product=1;
+
+for(var i=1; i<12; ++i){
+    product *= (i);
+}
+
+console.log(product*i);
+
+
